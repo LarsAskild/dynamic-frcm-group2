08ea9e14650fadde6fc8f491b30a2244285e03d2
LarsAskild <112076213+LarsAskild@users.noreply.github.com>
2024-03-14 12:39:52 +0100

    WebPage update
    Now possible to write in adress and compute fireRisk

--- src/frcm/weatherdata/app.py
+++ src/frcm/weatherdata/app.py
@@ -3,6 +3,7 @@ from geopy.geocoders import Nominatim
 
 app = Flask(__name__)
 geolocator = Nominatim(user_agent="geoapi")
+location_data = {}
 
 @app.route('/')
 def index():
@@ -13,6 +14,8 @@ def get_coordinates():
     address = request.args.get('address')
     location = geolocator.geocode(address)
     if location:
+        location_data['latitude'] = location.latitude
+        location_data['longitude'] = location.longitude
         return {
             'latitude': location.latitude,
             'longitude': location.longitude
@@ -20,5 +23,5 @@ def get_coordinates():
     else:
         return {'error': 'Address not found'}, 404
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+#if __name__ == '__main__':
+#   app.run(debug=True, port=8000)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,24 @@
 import datetime
+import threading
+import time
 
 from frcm.frcapi import FireRiskAPI
 from frcm.weatherdata.client_met import METClient
 from frcm.weatherdata.extractor_met import METExtractor
 from frcm.datamodel.model import Location
+from frcm.weatherdata.app import app, location_data
 
 # sample code illustrating how to use the Fire Risk Computation API (FRCAPI)
 if __name__ == "__main__":
 
     met_extractor = METExtractor()
+    #Start the web server
+    threading.Thread(target=lambda: app.run(port=8000, debug=True, use_reloader=False)).start()
+    while(not('latitude' in location_data and 'longitude' in location_data)):
+        time.sleep(0.2)
 
+    print(location_data)
+    
     # TODO: maybe embed extractor into client
     met_client = METClient(extractor=met_extractor)
 
@@ -17,6 +26,7 @@ if __name__ == "__main__":
 
     location = Location(latitude=60.383, longitude=5.3327)  # Bergen
     # location = Location(latitude=59.4225, longitude=5.2480)  # Haugesund
+    LocationGiven = Location(latitude=location_data['latitude'], longitude=location_data['longitude'])
 
     # Fails
     # location = Location(latitude=62.5780, longitude=11.3919)  # Røros
@@ -26,6 +36,7 @@ if __name__ == "__main__":
 
     obs_delta = datetime.timedelta(days=2)
 
-    predictions = frc.compute_now(location, obs_delta)
+    predictions = frc.compute_now(LocationGiven, obs_delta)
 
     print(predictions)
+    
