85730d1356ca53827ba785ddef6fc09db79ea564
sanderVatne <79106278+sanderVatne@users.noreply.github.com>
2024-03-21 14:27:02 +0100

    Added test_frost_api to integ tests

--- integ_tests/test.py
+++ integ_tests/test.py
@@ -2,23 +2,25 @@ import logging
 from datetime import date, datetime, timedelta
 from unittest import TestCase, main
 import os
+from frcm.datamodel.model import Location
+from frcm.weatherdata.client_met import METClient
 #from temp_store.client import MetClient
 #from temp_store.domain import Location
 #from temp_store.store import TemperatureStore
 
 
 class IntegTest(TestCase):
-    '''
+    
     def test_frost_api(self):
         client_id = os.getenv('MET_CLIENT_ID')
         assert client_id is not None
         lat, long = 60.36930,5.35000
         location = Location("hvl", long, lat)
-        client = MetClient(client_id)
+        client = METClient(client_id)
         result = client.retrieve(location,date(2024, 2, 8))
         self.assertTrue(isinstance(result, dict))
         self.assertTrue(result.values().__iter__().__next__(), float)
-
+    '''
     def test_full(self):
         client_id = os.getenv('MET_CLIENT_ID')
         assert client_id is not None
